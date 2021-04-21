a73943b5718bd712791f5a6e79b28e239bc5b6ab
MZ MEZBAH <mdmezbahuddinfeni@gmail.com>
2021-04-22 02:24:19 +0600

    add a method

--- app.js
+++ app.js
@@ -10,9 +10,14 @@ class Greetings{
     evening(name){
         console.log(`Good Evening, ${name}. What's going on?`)
     }
+
+    Greeting(){
+        console.log(`GOOD MORNING, GOOD AFTER NOON,  GOOD EVENING, GOOD NIGHT`)
+    }
 }
 
 let Greeting = new Greetings()
 
 console.log(Greeting.morning("mezvah"))
 console.log(Greeting.evening("mezvah"))
+console.log(Greeting.Greeting())
